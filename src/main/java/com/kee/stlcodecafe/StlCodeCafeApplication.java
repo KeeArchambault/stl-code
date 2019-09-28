daa4ca88e44f2d2e0c8a1e29917600dddba10e14
Kee <keeandraa@gmail.com>
2019-09-28 16:39:27 -0500

    initial commit

---
+++ src/main/java/com/kee/stlcodecafe/StlCodeCafeApplication.java
@@ -0,0 +1,13 @@
+package com.kee.stlcodecafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StlCodeCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StlCodeCafeApplication.class, args);
+	}
+
+}
