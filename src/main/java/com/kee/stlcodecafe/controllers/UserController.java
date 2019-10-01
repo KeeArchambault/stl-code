a295de607804aa9c8972b3897363b42dd16b78fe
Kee <keeandraa@gmail.com>
2019-09-30 22:55:34 -0500

    working on session logic

--- src/main/java/com/kee/stlcodecafe/controllers/UserController.java
+++ src/main/java/com/kee/stlcodecafe/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.kee.stlcodecafe.controllers;
 
 import com.kee.stlcodecafe.models.Post;
+import com.kee.stlcodecafe.models.Session;
 import com.kee.stlcodecafe.models.User;
 import com.kee.stlcodecafe.models.data.PostDao;
 import com.kee.stlcodecafe.models.data.SessionDao;
@@ -29,64 +30,70 @@ public class UserController {
     @Autowired
     private SessionDao sessionDao;
 
+
+
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String profile (Model model, @PathVariable int id){
-
-        model.addAttribute("user", userDao.findById(id));
-        model.addAttribute("title", "Profile");
-        return "profile/index";
-    }
-
-    @RequestMapping(value="/login", method = RequestMethod.GET)
-    public String login(Model model){
+    public String displayLogin(Model model) {
 
         model.addAttribute("title", "Log In");
         return "profile/login";
     }
 
-    @RequestMapping(value="/login", method = RequestMethod.POST)
-    public String processLogin(Model model, @RequestParam String username, String password){
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String processLogin(Model model, @RequestParam String username, String password) {
 
-    for(User user : userDao.findAll()){
-        if(username == user.getName()){
-            int targetId = user.getId();
+        for (User user : userDao.findAll()) {
+            if (username.equals(user.getName())) {
+                int targetId = user.getId();
 
-            sessionDao.save(user);
+                sessionDao.save(user);
 
-
-
+            }
         }
-    }
 
         model.addAttribute("title", "Log In");
         return "profile/login";
     }
 
-    @RequestMapping(value="sign-up", method = RequestMethod.GET)
-    public String signUp(Model model, User user){
+    @RequestMapping(value = "sign-up", method = RequestMethod.GET)
+    public String signUp(Model model, User user) {
 
         model.addAttribute("user", user);
         model.addAttribute("title", "Sign Up");
         return "profile/sign-up";
     }
 
-    @RequestMapping(value="sign-up", method= RequestMethod.POST)
+    @RequestMapping(value = "sign-up", method = RequestMethod.POST)
     public String processSignUp(Model model, @ModelAttribute @Valid User user, Errors errors) {
 
-        for(User existUser : userDao.findAll()) {
-            if (user.getId() != existUser.getId()){
-                if (errors.hasErrors() || user.getPassword() != user.getVerify()) {
+        for (User existUser : userDao.findAll()) {
+            if (user.getId() != existUser.getId()) {
+                if (errors.hasErrors() || !user.getPassword().equals(user.getVerify())) {
                     return "user/sign-up";
-                }else {int id = user.getId();
+                } else {
+                    int id = user.getId();
                     userDao.save(user);
                     return "redirect:/profile" + id;
 
 
                 }
-        }else {int id = user.getId();
-                    userDao.save(user);
-                    return "redirect:/profile" + id;
+            } else {
+                int id = user.getId();
+                sessionDao.save(user);
+                return "redirect:/profile" + id;
+            }
+
+        }
+        return "profile/sign-up";
     }
 
+    @RequestMapping(value = "profile", method = RequestMethod.GET)
+    public String profile(Model model, @RequestParam Session session) {
 
+        if(session.getUser() != null)
+
+        model.addAttribute("user", session.getUser());
+        model.addAttribute("title", "Profile");
+        return "profile/index";
+    }
 }
