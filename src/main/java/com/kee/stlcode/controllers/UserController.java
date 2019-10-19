97106b0b147bb8fe9f0fa36721499bfeeb6dbb1b
Kee <keeandraa@gmail.com>
2019-10-19 18:43:38 -0500

    changed profile pic path

--- src/main/java/com/kee/stlcode/controllers/UserController.java
+++ src/main/java/com/kee/stlcode/controllers/UserController.java
@@ -134,7 +134,7 @@ public class UserController extends AbstractController {
             model.addAttribute("profilePic", fileName);
         }else {
             String picName = pic.getName();
-            String profilePic = System.getProperty("user.dir") + "/src/main/resources/static/uploads/" + picName;
+            String profilePic = "/app/src/main/resources/static/uploads/" + picName;
             model.addAttribute("profilePic", profilePic);
         }
 
