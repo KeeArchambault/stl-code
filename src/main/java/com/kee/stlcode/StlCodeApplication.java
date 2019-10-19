8835175474bfeb9752c31a94bd77ba23a4051d2d
Kee <keeandraa@gmail.com>
2019-10-19 14:34:34 -0500

    removed mkdir

--- src/main/java/com/kee/stlcode/StlCodeApplication.java
+++ src/main/java/com/kee/stlcode/StlCodeApplication.java
@@ -1,12 +1,16 @@
 package com.kee.stlcode;
 
+import com.kee.stlcode.controllers.UploadController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.io.File;
 
 @SpringBootApplication
 public class StlCodeApplication {
+
 	public static void main(String[] args){
 		SpringApplication.run(StlCodeApplication.class, args);
 	}
-
 }
