13b236e5509024ef144770a7a26c4f58bf76e551
Kee <keeandraa@gmail.com>
2019-10-19 14:28:33 -0500

    added mkdir

--- src/main/java/com/kee/stlcode/StlCodeApplication.java
+++ src/main/java/com/kee/stlcode/StlCodeApplication.java
@@ -3,11 +3,12 @@ package com.kee.stlcode;
 import com.kee.stlcode.controllers.UploadController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.io.File;
 
 @SpringBootApplication
-
+@ComponentScan({"com.kee.stlcode", "UploadController"})
 public class StlCodeApplication {
 
 	public static void main(String[] args){
