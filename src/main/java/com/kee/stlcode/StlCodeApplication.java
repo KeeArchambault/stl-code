75e95f6a17995fa58fd997622ec6256d692df688
Kee <keeandraa@gmail.com>
2019-10-19 14:23:26 -0500

    added mkdir

--- src/main/java/com/kee/stlcode/StlCodeApplication.java
+++ src/main/java/com/kee/stlcode/StlCodeApplication.java
@@ -1,11 +1,17 @@
 package com.kee.stlcode;
 
+import com.kee.stlcode.controllers.UploadController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
+
 public class StlCodeApplication {
+
 	public static void main(String[] args){
+		new File(UploadController.uploadDirectory).mkdir();
 		SpringApplication.run(StlCodeApplication.class, args);
 	}
 
