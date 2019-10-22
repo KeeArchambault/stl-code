ba4c8fb6b8d4d9886faca9df1d58f1eb3c063fc3
Kee <keeandraa@gmail.com>
2019-10-22 03:22:28 -0500

    added image handler

--- src/main/java/com/kee/stlcode/controllers/ImageController.java
+++ src/main/java/com/kee/stlcode/controllers/ImageController.java
@@ -4,8 +4,10 @@ package com.kee.stlcode.controllers;
 import com.kee.stlcode.models.User;
 import org.hibernate.cache.spi.support.AbstractReadWriteAccess;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -19,19 +21,19 @@ import java.io.IOException;
 public class ImageController extends AbstractController{
 
 
-    @RequestMapping(value = "/imageDisplay", method = RequestMethod.GET)
-    public void showImage(HttpServletResponse response, HttpServletRequest request)
+    @RequestMapping(value = "/imageDisplay/{id}", method = RequestMethod.GET)
+    public void showImage(@PathVariable int id, HttpServletResponse response, HttpServletRequest request)
             throws ServletException, IOException {
 
-        User user = getUserFromSession(request.getSession());
+        User user = userDao.findById(id).get();
 
         File file = user.getProfilePic();
         //init array with file length
         byte[] bytesArray = new byte[(int) file.length()];
-//
-//        FileInputStream fis = new FileInputStream(file);
-//        fis.read(bytesArray); //read file into bytes[]
-//        fis.close();
+
+        FileInputStream fis = new FileInputStream(file);
+        fis.read(bytesArray); //read file into bytes[]
+        fis.close();
 
         response.setContentType("image/jpeg, image/jpg, image/png, image/gif");
         response.getOutputStream().write(bytesArray);
